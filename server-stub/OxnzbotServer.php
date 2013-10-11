5282c43b0af24f8db865de78dbc30da1ba4b2728
oxnz <yunxinyi@gmail.com>
2013-10-11 16:43:53 +0800

    rename and relayout

---
+++ server-stub/OxnzbotServer.php
@@ -0,0 +1,30 @@
+<?php
+/*
+     Filename: OxnzRobotServer.php
+  Description: 
+
+      Version: 0.1
+      Created: 2013-10-11 15:01:51
+  Last-update: 2013-10-11 15:29:10
+     Revision: None
+
+       Author: Oxnz
+        Email: yunxinyi@gmail.com
+
+Revision history:
+	Date Author Remarks
+*/
+
+//CONFIG:
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+
+class DBHelper {
+}
+
+class OxnzbotServer {
+}
+?>
