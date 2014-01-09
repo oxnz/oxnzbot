b94ab8db16ebf690f5d22b9f4e0d77a6cc5c7ce2
oxnz <yunxinyi@gmail.com>
2014-01-09 12:49:14 +0800

    add a server class to handle request

--- server-stub/OxnzbotServer.php
+++ server-stub/OxnzbotServer.php
@@ -25,6 +25,55 @@ define('DB_CHARSET', 'utf8');
 class DBHelper {
 }
 
-class OxnzbotServer {
+class CommandServer {
 }
+
+class ControlServer {
+}
+
+class OxnzbotServer {
+	private function handleGet() {
+		//echo "get<br>";
+		//var_dump($_GET);
+		echo "sleep 10";
+	}
+	private function handlePost() {
+		echo "post";
+		var_dump($_POST);
+	}
+	private function handleRequest() {
+		//var_dump($_SERVER);
+		//var_dump($_REQUEST);
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$this->handlePost();
+		} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+			$this->handleGet();
+		}
+	}
+	public function run() {
+		$this->handleRequest();
+	}
+}
+
+function main() {
+	try {
+		$server = new OxnzbotServer;
+		$server->run();
+	} catch (Exception $e) {
+		echo 'Exception: ', $e->getMessage(), '\n';
+	}
+}
+
+
+main();
+
 ?>
+
+<form method="post"
+	action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+	from: <input type="text" name="from" /><br />
+	command: <input type="text" name="command" /><br />
+	status: <input type="number" name="status" /><br />
+	output: <input type="text" name="output" /><br />
+	<input type="submit" value="Submit" />
+</form>
